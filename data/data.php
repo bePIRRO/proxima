5260d10dc80875627b86756d0a53a0d18adc2fab
bePIRRO <manuel.pirro@gmail.com>
2022-02-20 19:02:53 +0100

    first commit

---
+++ data/data.php
@@ -0,0 +1,44 @@
+<?php
+$location = [
+    [
+        "region" => "Emilia Romagna",
+        "province" => "RE",
+        "city" => "Reggio Emilia",
+        "zip_code" => 1,
+    ],
+
+    [
+        "region" => "Emilia Romagna",
+        "province" => "MO",
+        "city" => "Modena",
+        "zip_code" => 2,
+    ],
+
+    [
+        "region" => "Trentino Alto Adige",
+        "province" => "BZ",
+        "city" => "Bolzano",
+        "zip_code" => 3,
+    ],
+
+    [
+        "region" => "Trentino Alto Adige",
+        "province" => "BZ",
+        "city" => "Selva di Val Gardena",
+        "zip_code" => 4,
+    ],
+
+    [
+        "region" => "Veneto",
+        "province" => "VI",
+        "city" => "Bolzano Vicentino",
+        "zip_code" => 5,
+    ],
+
+    [
+        "region" => "Veneto",
+        "province" => "VR",
+        "city" => "Ronco all'Adige",
+        "zip_code" => 6,
+    ],
+];
